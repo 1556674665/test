2c4fc6ded470ead3e61c9249b221f9556a7b8103
肖成正 <chengzheng.xiao@eascs.com>
2022-11-24 14:39:48 +0800

    初始化

---
+++ src/main/java/com/example/test/controller/Test.java
@@ -0,0 +1,18 @@
+package com.example.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/test")
+@RestController
+public class Test {
+
+
+    @GetMapping("/api")
+    public String test() {
+        return "test";
+    }
+
+}
